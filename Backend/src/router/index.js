18bef8ed8faf261600f6370db8686245da3a95da
Johana-RHP <johana.rosicel@gmail.com>
2024-02-12 21:42:09 -0500

    delete files

--- Backend/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { authRouter } from "../components/index.js";
-
-const routes = [["auth", authRouter]];
-
-export const router = (app) => {
-    routes.forEach(([path, controller]) => {
-        app.use(`/api/v1/${path}`, controller);
-    });
-};
