14cad6cb4b4df19261a4133475a5cca213e9c1d0
Johana-RHP <johana.rosicel@gmail.com>
2024-02-13 07:00:58 -0500

    add specialties

---
+++ Frontend/src/components/Especialidades/Especialidades.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import './Style.css';
+const Especialidades = ({especialidad})=>{
+    return <div className='col-12 col-lg-4 d-flex align-items-center p-3'>
+        <img src="/images/logo-esp.svg" style={{ 
+                    width:"20px",
+                    height:"20px",
+            }} alt={especialidad} />
+        <h5 className='ms-3' style={{ 
+                    color: "#FAFAFA",
+                    fontFamily: "'Poppins', 'sans-serif'",
+                    fontWeight: '500',
+                    fontSize: '22px',
+                    lineHeight: '130%',
+                    
+            }}>{especialidad}</h5>
+    </div>
+};
+
+export default Especialidades;
