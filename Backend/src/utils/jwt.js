0b1da329a14dc6922e27c890e0aec419f74beb7e
Johana-RHP <johana.rosicel@gmail.com>
2024-02-12 22:41:52 -0500

    fix: async decoded token

---
+++ Backend/src/utils/jwt.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = async (token) => {
+    try {
+        const decoded = await jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+        return decoded;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const createToken = (data) =>
+    jwt.sign(data, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: process.env.JWT_ACCESS_SECRET_EXP,
+    });
+
+module.exports = { verifyToken, createToken };
