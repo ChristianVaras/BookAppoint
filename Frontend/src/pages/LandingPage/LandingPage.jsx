133be83ab06a0515871a5bfc94c754e23b40a3c4
ChristianVaras <christianvaras.2020@gmail.com>
2024-02-13 20:08:52 -0500

    Se agrego especialidades se mejoro la interacion

---
+++ Frontend/src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,28 @@
+import Carrusel from "../../components/Carrusel/Index";
+import QuienesSomos from "../../components/QuienesSomos/Index";
+//import ListaServicios from "../../components/Servicios/Servicios";
+import EspecialidadesItems from "../../components/Especialidades/EspecialidadesItems";
+import ContactUs from "../../components/ContactUs/ContactUs";
+import Entidades from "../../components/Entidades/Entidades";
+import Footer from "../../components/Footer/Footer";
+import ListaNoticias from "../../components/Noticias/ListaNoticias";
+import './styleLanding.css'
+import Servicios from "../../components/Servicios/Servicios";
+
+function LandingPage() {
+  return (
+    <>
+      <Carrusel />
+      <div className="desktop-responsive">
+        <QuienesSomos />
+      </div>
+        <Servicios />
+        <EspecialidadesItems />
+        <ListaNoticias />
+        <ContactUs />
+        <Entidades />
+        <Footer />
+    </>
+  );
+}
+export default LandingPage;
